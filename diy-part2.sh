620c3a508e06b76e15a10df23f5043ddb41300ba
Tiger <xiangtailiang@foxmail.com>
2020-06-12 11:38:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/fw876/helloworld package/SSR_Plus
+./scripts/feeds update -a
+./scripts/feeds install -a
